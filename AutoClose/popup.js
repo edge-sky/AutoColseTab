6ef7157d4f68857506074aef30fe9ec879a4de9c
edge_sky <Mingpu0465@gmail.com>
2024-02-03 19:21:51 +0800

    working in popup

--- AutoClose/popup.js
+++ AutoClose/popup.js
@@ -1,13 +1,22 @@
-chrome.storage.local.get(['closeTimes', 'closeNum', 'lastCloseNum'], function (data) {
+chrome.storage.local.get(['closeTimes', 'closeNum', 'lastCloseNum', 'isBlackList'], function (data) {
     document.getElementById('closeTimes').textContent += data.closeTimes;
     document.getElementById('closeNum').textContent += data.closeNum;
     document.getElementById('lastCloseNum').textContent += data.lastCloseNum;
+    if (data.isBlackList) {
+        document.getElementById('listSwitch').checked = false;
+        document.getElementById('displayList').innerText = '黑名单';
+    } else {
+        document.getElementById('listSwitch').checked = true;
+        document.getElementById('displayList').innerText = '白名单';
+    }
 });
 
 document.getElementById('listSwitch').addEventListener('change', function(event) {
-    if (event.target.value.equals('blackList')) {
-        chrome.storage.local.set({isBlackList: true});
-    } else {
+    if (event.target.checked) {
         chrome.storage.local.set({isBlackList: false});
+        document.getElementById('displayList').innerText = '白名单';
+    } else {
+        chrome.storage.local.set({isBlackList: true});
+        document.getElementById('displayList').innerText = '黑名单';
     }
 });
