ad700caa116dde202840dec0abe4969e20a66470
edge_sky <Mingpu0465@gmail.com>
2024-02-08 09:47:17 +0800

    turn listen tab's update to windows create

--- AutoClose/background.js
+++ AutoClose/background.js
@@ -1,73 +1,124 @@
-let isReady = true;
+// let isReady = true;
 
-chrome.windows.onRemoved.addListener(function (windowId) {
-    // close process complete
-    if (isReady) {
-        chrome.windows.getAll({}, function (windows) {
-            if (windows.length === 0) {
-                chrome.storage.local.set({newOpen: true});
-                chrome.storage.local.set({lastCloseNum: 0});
-            }
-        });
-    } else {
-        setTimeout(function () {
-            // waiting 0.5second
-        }, 500);
-    }
-});
+// chrome.windows.onRemoved.addListener(function (windowId) {
+//     // close process complete
+//     if (isReady) {
+//         chrome.windows.getAll({}, function (windows) {
+//             if (windows.length === 0) {
+//                 chrome.storage.local.set({newOpen: true});
+//                 chrome.storage.local.set({lastCloseNum: 0});
+//             }
+//         });
+//     } else {
+//         setTimeout(function () {
+//             // waiting 0.5second
+//         }, 500);
+//     }
+// });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.status === 'complete' && tab.active && isReady) {
-        isReady = false;
-        // get state of is new tab; it sees like run more than one time, it may a problem
-        chrome.storage.local.get(['newOpen', 'isBlackList', 'blackList', 'whiteList'], function (data) {
-            if (data.newOpen) {
-                // set state of new tab
-                chrome.storage.local.set({newOpen: false});
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     if (tab.status === 'complete' && tab.active && isReady) {
+//         isReady = false;
+//         // get state of is new tab; it sees like run more than one time, it may a problem
+//         chrome.storage.local.get(['newOpen', 'isBlackList', 'blackList', 'whiteList'], function (data) {
+//             if (data.newOpen) {
+//                 // set state of new tab
+//                 chrome.storage.local.set({newOpen: false});
+//
+//                 chrome.tabs.query({currentWindow: true}, function (tabs) {
+//                     if (data.isBlackList) {
+//                         // black list mode
+//                         for (let i = 0; i < tabs.length; i++) {
+//                             for (let j = 0; j < data.blackList.length; j++) {
+//                                 if (tabs[i].id !== tab.id && matchDomain(data.blackList[j], tabs[i].url)) {
+//                                     chrome.tabs.remove(tabs[i].id);
+//                                 }
+//                             }
+//                         }
+//                     } else {
+//                         let isMatch = false;
+//                         // white list mode
+//                         for (let i = 0; i < tabs.length; i++) {
+//                             for (let j = 0; j < data.whiteList.length; j++) {
+//                                 if (!matchDomain(data.whiteList[j], tabs[i].url)) {
+//                                     isMatch = true;
+//                                 }
+//                             }
+//                             if (!isMatch && tabs[i].id !== tab.id) {
+//                                 chrome.tabs.remove(tabs[i].id);
+//                             }
+//                         }
+//                     }
+//
+//                     chrome.storage.local.set({lastCloseNum: tabs.length - 1});
+//                     chrome.storage.local.get('closeTimes', function (data) {
+//                         chrome.storage.local.set({closeTimes: data.closeTimes + 1});
+//                     });
+//                     chrome.storage.local.get('closeNum', function (data) {
+//                         chrome.storage.local.set({closeNum: data.closeNum + tabs.length - 1});
+//                     });
+//                 });
+//             }
+//         });
+//         isReady = true;
+//     }
+// });
 
+chrome.windows.onCreated.addListener(function (window) {
+    // get all windows
+    chrome.windows.getAll({}, function (windows) {
+        // if only one window
+        if (windows.length === 1) {
+            chrome.storage.local.set({lastCloseNum: 0});
+
+            chrome.storage.local.get(['isBlackList', 'blackList', 'whiteList'], function (data) {
                 chrome.tabs.query({currentWindow: true}, function (tabs) {
+                    let closeNum = 0;
+                    // match list
                     if (data.isBlackList) {
                         // black list mode
-                        for (let i = 0; i < tabs.length; i++) {
+                        for (let i = 0; i < tabs.length - 1; i++) {
                             for (let j = 0; j < data.blackList.length; j++) {
-                                if (tabs[i].id !== tab.id && matchDomain(data.blackList[j], tabs[i].url)) {
+                                if (matchDomain(data.blackList[j], tabs[i].url)) {
                                     chrome.tabs.remove(tabs[i].id);
+                                    closeNum++;
                                 }
                             }
                         }
                     } else {
                         let isMatch = false;
                         // white list mode
-                        for (let i = 0; i < tabs.length; i++) {
+                        for (let i = 0; i < tabs.length - 1; i++) {
                             for (let j = 0; j < data.whiteList.length; j++) {
                                 if (!matchDomain(data.whiteList[j], tabs[i].url)) {
                                     isMatch = true;
                                 }
                             }
-                            if (!isMatch && tabs[i].id !== tab.id) {
+                            if (!isMatch) {
                                 chrome.tabs.remove(tabs[i].id);
+                                closeNum++;
                             }
                         }
                     }
 
-                    chrome.storage.local.set({lastCloseNum: tabs.length - 1});
+                    // set data
+                    chrome.storage.local.set({lastCloseNum: closeNum});
                     chrome.storage.local.get('closeTimes', function (data) {
                         chrome.storage.local.set({closeTimes: data.closeTimes + 1});
                     });
                     chrome.storage.local.get('closeNum', function (data) {
-                        chrome.storage.local.set({closeNum: data.closeNum + tabs.length - 1});
+                        chrome.storage.local.set({closeNum: data.closeNum + closeNum});
                     });
                 });
-            }
-        });
-        isReady = true;
-    }
+            });
+        }
+    });
 });
 
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason === 'install') {
         chrome.storage.local.set({
-            newOpen: true,
+            // newOpen: true,
             closeTimes: 0,
             closeNum: 0,
             lastCloseNum: 0,
